ee0c588577456f448ea42c84a2139195518f191d
fanyiaa <fanyiaa@163.com>
2020-10-12 22:52:45 +0800

    530. 二叉搜索树的最小绝对差

---
+++ src/com/company/Solution530.java
@@ -0,0 +1,28 @@
+package com.company;
+
+//530. 二叉搜索树的最小绝对差
+//https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+public class Solution530 {
+    private int result = Integer.MAX_VALUE;
+    private TreeNode preNode = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        //二叉查找树中，中间节点的值一定是其左右节点值的中间数，因此最小差别一定是在中间节点与左右节点之间
+        //中序遍历二叉查找树，每次比较当前节点与前一节点差值的绝对值与目前result中保存的最小值的大小，将较小的保存在result中
+        getMin(root);
+        return result;
+    }
+
+    private void getMin(TreeNode root){
+        if(root == null){
+            return;
+        }
+        getMin(root.left);
+        if(preNode != null)
+        {
+            result = Math.min(Math.abs(root.val - preNode.val), result);
+        }
+        preNode = root;
+        getMin(root.right);
+    }
+}
